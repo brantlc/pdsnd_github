d3c782ca9b7fb63c73d2ae4e96034fddfb7a0fe0
Corban Brantley <brantlc4@gmail.com>
2020-07-31 18:22:51 -0700

    Remove hint text from a comment line

--- bikeshare.py
+++ bikeshare.py
@@ -20,7 +20,7 @@ def get_filters():
         (str) day_of_week - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # get user input for city (chicago, new york city, washington)
     city = input("Choose a city (Chicago, New York City, or Washington):\n").lower()
 
     while city not in CITY_DATA:
